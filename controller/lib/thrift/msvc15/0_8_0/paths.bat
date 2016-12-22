512bbb5ed906287419ba7b1dd6cc04bfcbae3a77
U-JNPR\sagarc <sagarc@sagarc-T460.jnpr.net>
2016-12-22 13:11:31 -0800

    fix for thrift 0.8.0 with patches to compile/link in windows

---
+++ controller/lib/thrift/msvc15/0_8_0/paths.bat
@@ -0,0 +1,8 @@
+copy C:\contrail\windowsclean\windows\third_party\thrift-0.8.0\compiler\cpp\src\generate\t_rb_generator.cc    .\compiler\cpp\src\generate\
+copy c:\contrail\windowsclean\windows\third_party\thrift-0.8.0\compiler\cpp\src\thrifty.yy                    .\compiler\cpp\src\
+copy c:\contrail\windowsclean\windows\third_party\thrift-0.8.0\compiler\cpp\compiler.vcxproj                  .\compiler\cpp\
+copy c:\contrail\windowsclean\windows\third_party\thrift-0.8.0\lib\cpp\src\windows\config.h                   .\lib\cpp\src\windows\
+copy c:\contrail\windowsclean\windows\third_party\thrift-0.8.0\lib\cpp\src\windows\TWinsockSingleton.h        .\lib\cpp\src\windows\
+copy c:\contrail\windowsclean\windows\third_party\thrift-0.8.0\lib\cpp\src\Thrift.h                           .\lib\cpp\src\
+copy c:\contrail\windowsclean\windows\third_party\thrift-0.8.0\lib\cpp\libthrift.vcxproj                      .\lib\cpp\
+copy c:\contrail\windowsclean\windows\third_party\thrift-0.8.0\lib\cpp\libthriftnb.vcxproj                    .\lib\cpp\
